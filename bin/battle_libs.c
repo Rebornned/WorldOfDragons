cf0d80ee7cbdc5f99e2ab95722b12a237653d32f
Nosremehd <148909815+Rebornned@users.noreply.github.com>
2024-09-25 10:40:52 -0300

    new battle create

---
+++ bin/battle_libs.c
@@ -0,0 +1,13 @@
+#include "dlibs.h"
+
+#define True 0
+#define False 1
+typedef int bool;
+
+int causeDamage(int damage, int multiplicator, int precision, Dragon *enemy) {
+    int randomVector[100], choiceVector[precision], bool canHit = True;
+    srand(time(NULL));
+    
+    //for(int i)
+
+}
